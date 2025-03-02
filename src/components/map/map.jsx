44af01792a538164991fc3c53fd83f029c9a0bfa
Will Rutland <wrutland07@gmail.com>
2025-03-02 15:39:52 +0000

    working pan and zoom for map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import mapImage from '../../assets/images/rdr2Map.jpeg';
 import './mapStyles.scss';
 
@@ -6,33 +6,40 @@ export default function Map({shownItems}) {
 
     const randomIdHash = String(Math.random());
 
+    let globalScale = 0.3;
+
     //initialise x and y related variables
     let [deltaX, deltaY] = [0, 0];
-    const initialTranslation = [-450, 100];
+    const initialTranslation = [-3787, -2278];
     let [oldX, oldY] = [0, 0];
+
+    useEffect(() => {
+
+        //set the event listener for the image being scrolled over
+        document.getElementById(randomIdHash).addEventListener('wheel', (event) => {imageScrolled(event), {passive: false}});
+    }, []);
 
     return (
         <React.Fragment>
             <div className="mapWrapper">
-                <img className="map" id={randomIdHash} src={mapImage} onDragStart={(event) => {dragStarted(event)}} onDrag={(event) => {imageDragged(event)}} style={{transform: `translate(${initialTranslation[0]}px, ${initialTranslation[1]}px)`}} />
+                <img className="map" id={randomIdHash} src={mapImage} onDragStart={(event) => {dragStarted(event)}} onDrag={(event) => {imageDragged(event)}} style={{translate: `${initialTranslation[0]}px ${initialTranslation[1]}px`, transform: `scale(${globalScale})`}} />
             </div>
         </React.Fragment>
     );
 
     function dragStarted(event) {
         
-        //set oldX and oldY as the coordinate at the point of the drag starting
+        //set oldX and oldY as the coordinate at the point of the drag starting, stops map teleporting at the star of a drag
         oldX = event.clientX;
         oldY = event.clientY;
     };
 
     function imageDragged(event) {
-        event.preventDefault();
 
         //calculate the change in position
         const [mouseX, mouseY] = [event.clientX, event.clientY];
-        deltaX = mouseX - oldX;
-        deltaY = mouseY - oldY;
+        deltaX = (mouseX - oldX);
+        deltaY = (mouseY - oldY);
         [oldX, oldY] = [mouseX, mouseY];
 
         //if the drag is stopped, stop the map from teleporting
@@ -41,13 +48,32 @@ export default function Map({shownItems}) {
         };
 
         //calculate the amount to move the image by
-        let [currentX, currentY] = event.target.style.transform.replace('translate(', '').replace(')', '').split(', ');
+        let [currentX, currentY] = event.target.style.translate.split(' ');
         currentX = Number(currentX.replace('px', ''));
         currentY = Number(currentY.replace('px', ''));
         let [newX, newY] = [(currentX+deltaX), (currentY+deltaY)];
 
         //update the position of the image
         const image = event.target;
-        image.style.transform = `translate(${newX}px, ${newY}px)`;
+        image.style.translate = `${newX}px ${newY}px`;
+    };
+
+    function imageScrolled(event) {
+        event.preventDefault();
+
+        const imageScale = event.target.style.transform.replace('scale(', '').replace(')', '');
+
+        //calculate scale factor
+        const newScale = Number(imageScale) + (event.deltaY / 1000);
+
+        //make sure newScale stays within bounds
+        const bounds = [0.3, 3];
+        if (newScale > bounds[1] || newScale < bounds[0]) {
+            return;
+        };
+
+        //scale the image
+        globalScale = newScale;
+        event.target.style.transform = `scale(${newScale})`;
     };
 };
